45bbe7d36d1415d7acf56afec46ad8eb2cc43ae6
sajib hossen <sajibhossen560@gmail.com>
2025-10-11 11:47:48 +0600

    all project get backend

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import { BsGlobe } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa6";
+import { useProjects } from "../context/Project.context.jsx";
+// const projects = [
+//   {
+//     id: 1,
+//     title: "E-Commerce Website",
+//     desc: "A full-featured MERN e-commerce platform with product management, cart, and admin panel.",
+//     tech: ["React", "Node.js", "MongoDB", "Express.js"],
+//     link: "https://yourlink.com",
+//     github: "https://github.com/yourgithub/ecommerce",
+//   },
+//   {
+//     id: 2,
+//     title: "Portfolio Website",
+//     desc: "A personal portfolio showcasing skills and projects, built with React and Tailwind CSS.",
+//     tech: ["React", "Tailwind", "Vite"],
+//     link: "#",
+//     github: "https://github.com/joysof/portfolio",
+//   },
+//   {
+//     id: 3,
+//     title: "Task Manager App",
+//     desc: "A simple task management app with user authentication and real-time updates.",
+//     tech: ["React", "Node.js", "MongoDB" ,"Express.js"],
+//     link: "https://bd-calling-assessment-frontend.onrender.com",
+//     github: "https://github.com/joysof/bd_Calling_assessment",
+//   },
+// ];
 
-const projects = [
-  {
-    id: 1,
-    title: "E-Commerce Website",
-    desc: "A full-featured MERN e-commerce platform with product management, cart, and admin panel.",
-    tech: ["React", "Node.js", "MongoDB", "Express.js"],
-    link: "https://yourlink.com",
-    github: "https://github.com/yourgithub/ecommerce",
-  },
-  {
-    id: 2,
-    title: "Portfolio Website",
-    desc: "A personal portfolio showcasing skills and projects, built with React and Tailwind CSS.",
-    tech: ["React", "Tailwind", "Vite"],
-    link: "#",
-    github: "https://github.com/joysof/portfolio",
-  },
-  {
-    id: 3,
-    title: "Task Manager App",
-    desc: "A simple task management app with user authentication and real-time updates.",
-    tech: ["React", "Node.js", "MongoDB" ,"Express.js"],
-    link: "https://bd-calling-assessment-frontend.onrender.com",
-    github: "https://github.com/joysof/bd_Calling_assessment",
-  },
-];
+
 
 const Projects = () => {
+  const { projects, loading } = useProjects();
+  console.log("teechStack" ,projects.techStack)
+   if (loading) return <p>Loading projects...</p>;
   return (
     <section
       id="projects"
@@ -41,21 +46,21 @@ const Projects = () => {
       </h2>
 
       <div className=" md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 gap-8 px-6 md:px-16">
-        {projects.map((p) => (
+        {projects.map((project ,) => (
           <div
-            key={p.id}
+            key={project._id}
             className="bg-white/70 dark:bg-gray-800/50 backdrop-blur-lg border border-gray-200 
             dark:border-gray-700 rounded-2xl p-6 shadow-lg hover:shadow-2xl hover:-translate-y-2 
             transition-all duration-500 my-10"
           >
             <h3 className="text-xl md:text-2xl font-semibold text-gray-800 dark:text-gray-100 mb-3">
-              {p.title}
+              {project.title}
             </h3>
-            <p className="text-gray-600 dark:text-gray-300 text-sm  md:text-[16px] mb-4">{p.desc}</p>
+            <p className="text-gray-600 dark:text-gray-300 text-sm  md:text-[16px] mb-4">{project.desc}</p>
 
             {/* Tech stack badges */}
             <div className="flex flex-wrap justify-center gap-2 mb-5">
-              {p.tech.map((t, i) => (
+              {project.techStack.map((t, i) => (
                 <span
                   key={i}
                   className="bg-gradient-to-r from-teal-500 to-blue-500 text-white px-3 py-1 
@@ -69,7 +74,7 @@ const Projects = () => {
             
             <div className="flex gap-4 justify-center items-center mt-6">
               <a
-                href={p.link}
+                href={project.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center justify-center gap-2 px-3 py-2 sm:px-5 lg:px-8  md:py-3 bg-gradient-to-r from-teal-500 to-blue-500 
@@ -81,7 +86,7 @@ const Projects = () => {
               </a>
 
               <a
-                href={p.github}
+                href={project.github}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex  items-center justify-center px-3 py-2 gap-2 sm:px-5 lg:px-8 sm:py-3 border-2 border-teal-500 
