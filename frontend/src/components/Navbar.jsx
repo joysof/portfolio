54f1df99be5eb849e2e441791642e249c0df4210
sajib hossen <sajibhossen560@gmail.com>
2025-10-11 23:30:48 +0600

    update package.json

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <ul className="hidden md:flex space-x-8">
           {navLinks.map((link) => (
-            <li key={link.name} className="relative group">
+            <li key={link.name} className="relative ">
               <a
                 href={link.href}
                  className="px-6 py-2 rounded-full text-gray-800 dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-800 hover:text-blue-600 dark:hover:text-blue-200 transition-all duration-300"
